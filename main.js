2a22b59cf43c5fc0950bfe7239c4f339c9f30d49
http-Figueiredo <http.figueiredo@gmail.com>
2026-01-09 07:36:05 -0400

    feat: added JS code for functionality

---
+++ main.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const taskInput = document.getElementById("task-input");
+    const addTaskbtn = document.getElementById("add-task-btn");
+    const taskList = document.getElementById("task-list");
+
+
+    const addTask = (event) => {
+        event.preventDefault();
+        const taskText = taskInput.value.trim();
+        if (!taskText) {
+            return
+        }
+        
+        const li = document.createElement("li");
+        
+        li.innerHTML = `
+            <input type="checkbox" class="checkbox">
+            <span>${taskText}</span>
+            `;
+
+        taskList.appendChild(li);
+        taskInput.value = '';
+    }
+
+    addTaskbtn.addEventListener('click', addTask);
+    taskInput.addEventListener('keypress', (e) => {
+        if(e.key === 'Enter') {
+            addTask(e);
+        };
+    });
+});
